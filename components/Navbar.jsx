33b7d0fdde3171c302b1dadfba6f52b9723e62d4
Kassim <kassimbouzoubaa05@gmail.com>
2023-03-07 10:12:23 +0100

    compiled

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,33 @@
 'use client';
 
-import {motion} from "framer-motion"
+import { motion } from 'framer-motion';
 
-import styles from '../styles'
+import styles from '../styles';
 
-import {navVariants } from "../utils/motion"
- 
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
   <motion.nav
-  variants={navVariants}
-  initial="hidden"
-  whileInView="show"
-  className={`${styles.xPaddings} py-8 relative`}
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img className="w-[24px] h-[24px] object-contain " src="/search.svg" alt="search" />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">METAVERSE</h2>
-      <img className="w-[24px] h-[24px] object-contain" src="/menu.svg" alt="menu" />
+      <img
+        className="w-[24px] h-[24px] object-contain"
+        src="/search.svg"
+        alt="search"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        METAVERSE
+      </h2>
+      <img
+        className="w-[24px] h-[24px] object-contain"
+        src="/menu.svg"
+        alt="menu"
+      />
     </div>
   </motion.nav>
 );
